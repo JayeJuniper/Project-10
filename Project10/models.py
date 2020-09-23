6805ad4c78fe2ab88f929f860f74079236e32461
Jonathan Grenz <32690541+JonathanGrenz@users.noreply.github.com>
2020-09-22 22:55:14 -0700

    Update models.py
    removed unused imports and code left over from the user resource that is no longer part of the project.

--- Project10/models.py
+++ Project10/models.py
@@ -1,20 +1,16 @@
 import datetime
 
-from argon2 import PasswordHasher
-from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer,
-                          BadSignature, SignatureExpired)
 from peewee import *
 
 import config
 
+
 DATABASE = SqliteDatabase('todos.sqlite')
-HASHER = PasswordHasher()
 
 
 class Todo(Model):
     name = CharField()
     created_at = DateTimeField(default=datetime.datetime.now)
-    #Created_by = ForeignKeyField(User, related_name='todo_set')
 
     class Meta:
         database = DATABASE
